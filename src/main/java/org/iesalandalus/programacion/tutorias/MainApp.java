0c7e02c87fffede1b97b54fd7faa8095581e3cd1
Mar <marmembriveplaza@gmail.com>
2020-04-27 11:42:41 +0200

    Menú básico app.

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -11,11 +11,23 @@ import org.iesalandalus.programacion.tutorias.mvc.vista.IVista;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Programa para la gestión de tutorías v3 del IES Al-Ándalus");
+		System.out.println("Programa para la gestión de tutorías v4 del IES Al-Ándalus");
 		IModelo modelo = new Modelo(FactoriaFuenteDatos.FICHEROS.crear());
-		IVista vista = FactoriaVista.TEXTO.crear();
+		IVista vista = procesarArgumentos(args);
 		IControlador controlador = new Controlador(modelo, vista);
 		controlador.comenzar();
+	}
+
+	private static IVista procesarArgumentos(String[] args) {
+		IVista vista = FactoriaVista.IUGRAFICA.crear();
+		for (String argumento : args) {
+			if (argumento.equalsIgnoreCase("-vgrafica")) {
+				vista = FactoriaVista.IUGRAFICA.crear();
+			} else if (argumento.equalsIgnoreCase("-vtexto")) {
+				vista = FactoriaVista.TEXTO.crear();
+			}
+		}
+		return vista;
 	}
 
 }
